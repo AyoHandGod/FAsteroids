555c981d5155bdba41cb5f38bb8f63ff9767c79d
Dayo <admin@nerdroco.com>
2018-11-08 12:40:58 -0800

    R & R
    Added Rotation and Refactored

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// TODO 
+// Resolve Ship rotating without input issue
+
 /// <summary>
 /// Script to Manage Ship gameObject
 /// </summary>
@@ -9,9 +13,10 @@ public class Ship : MonoBehaviour {
 
     #region Fields
     const float ThrustForce = 3f;
+    const float RotateDegreesPerSecond = 90f;
     Rigidbody2D rb2d;
     CircleCollider2D cc2d;
-    Vector2 thrustDirection = new Vector2(-1, 0);
+    Vector2 thrustDirection = new Vector2(1, 0);
     #endregion
 
     #region Methods
@@ -24,34 +29,60 @@ public class Ship : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Rotation();
+    }
 
     // Fixed update for more precise update
     private void FixedUpdate()
     {
-        float thrustMove = Input.GetAxis("Thrust");
-        if(thrustMove != 0)
-        {
-            rb2d.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force); 
-        }
+        ThrustCheck();
     }
 
     private void OnBecameInvisible()
     {
+        ScreenWrap();
+    }
+
+    private void Rotation()
+    {
+        float rotationInput = Input.GetAxis("Rotate");
+
+        // calculate rotation amount and apply rotation
+        float rotationAmount = RotateDegreesPerSecond * Time.deltaTime;
+        if (rotationInput > 0)
+        {
+            rotationAmount *= -1;
+            transform.Rotate(Vector3.forward, rotationAmount);
+        }
+        else if (rotationInput < 0)
+        {
+            transform.Rotate(Vector3.forward, rotationAmount);
+        }
+    }
+
+    private void ThrustCheck()
+    {
+        float thrustMove = Input.GetAxis("Thrust");
+        if (thrustMove != 0)
+        {
+            rb2d.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force);
+        }
+    }
+
+    private void ScreenWrap()
+    {
         Vector3 location = transform.position;
-        if(location.x > ScreenUtils.ScreenRight || location.y > ScreenUtils.ScreenTop)
+        if (location.x > ScreenUtils.ScreenRight || location.y > ScreenUtils.ScreenTop)
         {
             location.x = -location.x;
             location.y = -location.y;
         }
-        else if(location.x < ScreenUtils.ScreenLeft || location.y < ScreenUtils.ScreenBottom)
+        else if (location.x < ScreenUtils.ScreenLeft || location.y < ScreenUtils.ScreenBottom)
         {
             location.x = -location.x;
             location.y = -location.y;
         }
         transform.position = location;
     }
-
     #endregion
 }
