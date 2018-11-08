1cd42d253c34dfa038146c98c1e9d49003b118a6
Dayo <admin@nerdroco.com>
2018-11-08 12:12:58 -0800

    Screen Wrap
    Updated Ship script to wrap around screen when it becomes invisible

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -10,7 +10,8 @@ public class Ship : MonoBehaviour {
     #region Fields
     const float ThrustForce = 3f;
     Rigidbody2D rb2d;
-    Vector2 thrustDirection = new Vector2(1, 0);
+    CircleCollider2D cc2d;
+    Vector2 thrustDirection = new Vector2(-1, 0);
     #endregion
 
     #region Methods
@@ -18,6 +19,7 @@ public class Ship : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        cc2d = GetComponent<CircleCollider2D>();
 	}
 	
 	// Update is called once per frame
@@ -33,6 +35,22 @@ public class Ship : MonoBehaviour {
         {
             rb2d.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force); 
         }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Vector3 location = transform.position;
+        if(location.x > ScreenUtils.ScreenRight || location.y > ScreenUtils.ScreenTop)
+        {
+            location.x = -location.x;
+            location.y = -location.y;
+        }
+        else if(location.x < ScreenUtils.ScreenLeft || location.y < ScreenUtils.ScreenBottom)
+        {
+            location.x = -location.x;
+            location.y = -location.y;
+        }
+        transform.position = location;
     }
 
     #endregion
